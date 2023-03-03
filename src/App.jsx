3d0d09b101fb73661421217a2a3c1ff5e32a4413
imbasri <imbasri.dev@gmail.com>
2023-03-04 04:34:39 +0700

    setup vite react flowbite tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function App() {
+   return (
+      <>
+         <div className="w-full h-full bg-red-300">
+            <div className="bg-slate-200 h-screen max-w-[1440px] mx-auto flex items-center justify-center">
+               <h1 className="  w-full text-center font-bold text-6xl">
+                  Hello 😺
+               </h1>
+            </div>
+         </div>
+      </>
+   );
+}
+export default App;
